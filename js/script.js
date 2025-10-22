4d1d5fc9e0092a242423729d2e9e1ab4cb879c8b
CarterWilliamson08 <carterwilliamson8@gmail.com>
2025-10-22 13:59:45 -0400

    Began script.js work

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("script.js connected!");
+
+//Question 1
+
+let buttons = document.querySelectorAll("#question-1 button");
+console.log(buttons);
+
+//Checks for button click
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        console.log("button clicked");
+    });
+});
+
