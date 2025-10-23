9047b60cb9324b4834850815af47c6ed8627a315
CarterWilliamson08 <carterwilliamson8@gmail.com>
2025-10-23 11:15:10 -0400

    added questions, worked on JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 console.log("script.js connected!");
 
-//Question 1
-
-let buttons = document.querySelectorAll("#question-1 button");
+//Select all questions within the block
+let buttons = document.querySelectorAll(".question-block button");
 console.log(buttons);
 
+//Question 1
+let quesOne = document.querySelectorAll("#question-1 .answer-btn");
+console.log(quesOne);
+
+
 //Checks for button click
+
+
 buttons.forEach(function(button) {
     button.addEventListener("click", function() {
         console.log("button clicked");
